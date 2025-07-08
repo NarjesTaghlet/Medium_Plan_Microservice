c9fcc6853334d1d609468a390833709807dfba33
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-07-08 15:59:44 +0100

    first commit

--- terraform/MediumPlan/DEV/variables.tf
+++ terraform/MediumPlan/DEV/variables.tf
@@ -102,13 +102,7 @@ variable "disk_name" {
      }  
 
 
-/*  variable "github_pat" {
-  description = "GitHub Personal Access Token for CodeBuild"
-  type        = string
-  sensitive   = true
- default = "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"
-}
-*/
+
 
 
 
