0f594b961f279498a5783df626468a9cd5a9f466
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-07-16 12:08:15 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import crypto from 'crypto';
-global.crypto = require("crypto")
+(global as any).crypto = crypto;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //const app = await NestFactory.create(AppModule);
